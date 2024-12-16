5f36f1f6b7975405dae1d38714a28cdfd85ebbba
Chris Gomez <christiangomezdev@gmail.com>
2024-12-17 04:16:19 +0800

    feat: Setup and fix for server deployment
    - Setting up and fixing for server deployment

--- apps/api/index.js
+++ apps/api/index.js
@@ -1,1 +1,7 @@
-module.exports = require("./dist/app");
+const { startServer } = require("./dist/server");
+
+const PORT = process.env.PORT || 4000;
+
+startServer(PORT).catch((error) => {
+    console.error("Failed to start the server:", error);
+});
--- apps/api/src/server.ts
+++ apps/api/src/server.ts
@@ -1,4 +1,3 @@
-import "module-alias/register";
 import dotEnvt from "dotenv";
 
 dotEnvt.config();
@@ -8,14 +7,23 @@ import { sequelize } from "./database";
 
 const PORT = process.env.PORT || 4000;
 
-sequelize
-    .authenticate()
-    .then(() => {
-        console.log("Database connection established successfully.");
-        app.listen(PORT, () => {
-            console.log(`Server running on port ${PORT}`);
+const startServer = () => {
+    sequelize.sync();
+    sequelize
+        .authenticate()
+        .then(() => {
+            console.log("Database connection established successfully.");
+            app.listen(PORT, () => {
+                console.log(`Server running on port ${PORT}`);
+            });
+        })
+        .catch((error) => {
+            console.error("Error establishing database connection:", error);
         });
-    })
-    .catch((error) => {
-        console.error("Error establishing database connection:", error);
-    });
+};
+
+if (require.main === module) {
+    startServer();
+}
+
+export { startServer };
